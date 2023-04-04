7fd1833bf0f035d2e1cb29975eae22fb31c81acf
Leon Windhorst <leon@windhorst.dev>
2023-04-04 15:05:53 +0200

    doc: add comments to path of building package

--- packages/path-of-building/default.nix
+++ packages/path-of-building/default.nix
@@ -5,9 +5,8 @@
 
 let
   cmd = "path-of-building";
-  homePath = "/home/leon";
-  buildPathWin = "C:\\ProgramData\\PathOfBuilding\\Builds";
-  buildPathUnix = ".wine/drive_c/ProgramData/PathOfBuilding/Builds";
+  dataPathWin = "C:\\ProgramData\\PathOfBuilding\\Builds";
+  dataPathUnix = ".wine/drive_c/ProgramData/PathOfBuilding/Builds";
 
 in stdenvNoCC.mkDerivation rec {
   pname = "path-of-building";
@@ -28,58 +27,62 @@ in stdenvNoCC.mkDerivation rec {
     mkdir -p $out/etc
     cp -r $src/* $out/etc
 
-    # The build path has to be set to a writeable directory. 
-    # In this case we use the wine C:\ drive, which is located at $HOME/.wine/drive_c/.
+    # The build path has to be set to a writeable directory via PoB's Settings.xml. 
+    # In this case we use a dir on the wine C:\ drive, which is located at $HOME/.wine/drive_c/.
     echo "\
-    <?xml version=\"1.0\" encoding=\"UTF-8\"?>
-    <PathOfBuilding>
+      <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <PathOfBuilding>
         <Mode mode=\"LIST\">
         </Mode>
         <Accounts lastAccountName=\"Taikutan\" lastRealm=\"PC\">
-            <Account accountName=\"Taikutan\"/>
+          <Account accountName=\"Taikutan\"/>
         </Accounts>
         <SharedItems/>
         <Misc 
-            betaTest=\"false\" 
-            connectionProtocol=\"0\" 
-            thousandsSeparator=\",\" 
-            showThousandsSeparators=\"true\" 
-            slotOnlyTooltips=\"true\" 
-            invertSliderScrollDirection=\"false\" 
-            showTitlebarName=\"true\" 
-            disableDevAutoSave=\"false\" 
-            POESESSID=\"\" 
-            defaultCharLevel=\"1\" 
-            nodePowerTheme=\"RED/BLUE\" 
-            buildSortMode=\"EDITED\" 
-            showWarnings=\"true\" 
-            defaultItemAffixQuality=\"0.5\" 
-            defaultGemQuality=\"0\" 
-            decimalSeparator=\".\" 
-            buildPath=\"${buildPathWin}\\\"
+          betaTest=\"false\" 
+          connectionProtocol=\"0\" 
+          thousandsSeparator=\",\" 
+          showThousandsSeparators=\"true\" 
+          slotOnlyTooltips=\"true\" 
+          invertSliderScrollDirection=\"false\" 
+          showTitlebarName=\"true\" 
+          disableDevAutoSave=\"false\" 
+          POESESSID=\"\" 
+          defaultCharLevel=\"1\" 
+          nodePowerTheme=\"RED/BLUE\" 
+          buildSortMode=\"EDITED\" 
+          showWarnings=\"true\" 
+          defaultItemAffixQuality=\"0.5\" 
+          defaultGemQuality=\"0\" 
+          decimalSeparator=\".\" 
+          buildPath=\"${dataPathWin}\\\"
         />
-    </PathOfBuilding>
+      </PathOfBuilding>
     " > $out/etc/Settings.xml
 
+
+    # This is the script that will be added to PATH.
+    # It will run PoB via wine and create the data dir if it doesn't exist yet.
     mkdir -p $out/bin
     echo "\
-    ${wineWowPackages.stableFull}/bin/wine $out/etc/Path\ Of\ Building.exe &
-
-    if [ ! -d ~/${buildPathUnix} ]; then
-        mkdir -p ~/${buildPathUnix}
-    fi
+      ${wineWowPackages.stableFull}/bin/wine $out/etc/Path\ Of\ Building.exe &
+      if [ ! -d ~/${dataPathUnix} ]; then
+        mkdir -p ~/${dataPathUnix}
+      fi
     " > $out/bin/${cmd}
     chmod +x $out/bin/${cmd}
 
+
+    # Add a desktop file for PoB so it can be launched as a desktop app.
     mkdir -p $out/share/applications
     echo "\
-    [Desktop Entry]
-    Encoding=UTF-8
-    Version=${version}
-    Type=Application
-    Terminal=false
-    Exec=$out/bin/${cmd}
-    Name=Path of Building
+      [Desktop Entry]
+      Encoding=UTF-8
+      Version=${version}
+      Type=Application
+      Terminal=false
+      Exec=$out/bin/${cmd}
+      Name=Path of Building
     " > $out/share/applications/path-of-building.desktop
   '';
 }
