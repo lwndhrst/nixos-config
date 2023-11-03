5f7c5f41d3c8492dc48c7a0ea5052b04eada4d29
Leon Windhorst <leon@windhorst.dev>
2023-11-03 11:30:48 +0100

    chore: update laptop video driver config

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -44,16 +44,11 @@
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # Vulkan support
   hardware.opengl = {
     enable = true;
+
+    # Vulkan support
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      intel-media-driver  # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel          # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
   };
 }
