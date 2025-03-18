91005f263217c1c60dd63cd38a4eac7d5e1aa93f
Leon Windhorst <leon@windhorst.dev>
2025-03-18 17:39:05 +0100

    Update flake inputs

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -9,9 +9,10 @@
     polarity = "dark";
 
     cursor = {
-      # package = pkgs.bibata-cursors;
-      # name = "Bibata-Modern-Ice";
-      size = 24;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 22;
+      # size = 24;
     };
 
     fonts = {
