a3576fd1f83f00957e97e7e9b52107a6158ea4d2
Leon Windhorst <leon@windhorst.dev>
2025-10-14 08:31:23 +0200

    Remove old unused modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,29 +1,15 @@
 {
-  lightdm   = ./lightdm;
-
-  # Apps
-  android   = ./android;
-  flameshot = ./flameshot;
   fzf       = ./fzf;
   ghostty   = ./ghostty;
   gtk       = ./gtk;
   hypr      = ./hypr;
   kitty     = ./kitty;
   neovim    = ./neovim;
-  picom     = ./picom;
   rclone    = ./rclone;
-  rofi      = ./rofi;
   tmux      = ./tmux;
   tofi      = ./tofi;
   wezterm   = ./wezterm;
   zathura   = ./zathura;
   zotero    = ./zotero;
   zsh       = ./zsh;
-
-  # Languages, Toolchains, LSP, ...
-  c         = ./c;
-  odin      = ./odin;
-  rust      = ./rust;
-  tex       = ./tex;
-  zig       = ./zig;
 }
